dd02c7d0695b9c2762d4cef615995424fcd6c138
themoffat28 <bthemoffat@gmail.com>
2016-09-27 08:23:09 -0600

    Introduced JOptionPane / JavaDoc and corrected start function.

--- src/popups/controller/PopupRunner.java
+++ src/popups/controller/PopupRunner.java
@@ -2,5 +2,9 @@ package popups.controller;
 
 public class PopupRunner
 {
-
+	public static void main (String [] args)
+	{
+		PopupController popupAppController = new PopupController();
+		popupAppController.start();
+	}
 }
---
+++ src/popups/view/PopupViewer.java
@@ -0,0 +1,17 @@
+package popups.view;
+
+import javax.swing.JOptionPane;
+
+public class PopupViewer
+{
+	
+	/**
+	 * Makes a popup window to display the supplied message.
+	 * @param message : The message to be placed in the popup.
+	 */
+
+	public void displayMessage(String message)
+	{
+		JOptionPane.showMessageDialog(null, message);
+	}
+}
