734281222981562d767452a6e9c1bfc534dfa3f9
themoffat28 <bthemoffat@gmail.com>
2016-09-27 08:01:53 -0600

    Created getters and setters for all data members in the model package. (number, value, words, isValid)

---
+++ src/popups/model/PopupThingy.java
@@ -0,0 +1,46 @@
+package popups.model;
+
+public class PopupThingy
+{
+	private int number;
+	private double value;
+	private String words;
+	private boolean isValid;
+	
+	
+	public int getNumber()
+	{
+		return number;
+	}
+	public void setNumber(int number)
+	{
+		this.number = number;
+	}
+	public double getValue()
+	{
+		return value;
+	}
+	public void setValue(double value)
+	{
+		this.value = value;
+	}
+	public String getWords()
+	{
+		return words;
+	}
+	public void setWords(String words)
+	{
+		this.words = words;
+	}
+	public boolean isValid()
+	{
+		return isValid;
+	}
+	public void setValid(boolean isValid)
+	{
+		this.isValid = isValid;
+	}
+	
+	
+}
+	
