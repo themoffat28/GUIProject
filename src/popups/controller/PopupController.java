52030126b4f050570995658a0f9838df4ec44e7e
themoffat28 <bthemoffat@gmail.com>
2016-09-27 09:48:15 -0600

    Created popup and protected it against null response.

--- src/popups/controller/PopupController.java
+++ src/popups/controller/PopupController.java
@@ -1,10 +1,27 @@
 package popups.controller;
 
+import popups.view.PopupViewer;
+
 public class PopupController
 {
 	
+	private PopupViewer display;
+	
+	public PopupController()
+	{
+		display = new PopupViewer();
+	}
+	
 	public void start()
 	{
+		String answer = "sample";
+		while(answer != null && !answer.equals(""))
+		{
+			display.displayMessage("*Child Screams*");
+		    answer = display.collectResponse("ARE WE THERE YET?");
+			display.displayMessage("You tell the child " + answer);
+		
+		}
 		
 	}
 
