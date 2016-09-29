425748baa9c95b277d006a6b443eaeaf53b1593f
themoffat28 <bthemoffat@gmail.com>
2016-09-29 08:25:22 -0600

    Added a method to test double parsing.

--- src/popups/controller/PopupController.java
+++ src/popups/controller/PopupController.java
@@ -22,7 +22,32 @@ public class PopupController
 			display.displayMessage("You tell the child " + answer);
 		
 		}
-		
 	}
+		
+	/**
+	 * Checks if the supplied String can be parsed to a double value.
+	 * @param potentialValue : The string to test.
+	 * @return Whether the value could be parsed.
+	 */
 
+	private boolean isDouble(String potentialValue)
+	{
+		boolean isParseble = false;
+		
+		try
+		{
+			double test = Double.parseDouble(potentialValue);
+			isParseble = true;
+		}
+		catch(NumberFormatException notDouble)
+		{
+			display.displayMessage("This is not a double value! ");
+		}
+		
+		return isParseble; 
+	}
+	
+	
+	
+	
 }
